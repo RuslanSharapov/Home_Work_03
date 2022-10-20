14943756e694db835dcd2a9b1408261e56c8c46f
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-20 18:42:39 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ work_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+
+for (i = 1; i <= N; i++)
+{
+    int number = i * i * i;
+    if (number % 2 == 0)
+    {
+        Console.WriteLine($" {i} ^ 3 = {number} ");
+    }
+}
