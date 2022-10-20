c451236443e0afd72a53542c5572f8001f4bc2a4
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-20 18:02:45 +0300

    Программа вычисления чисел от 1 до N

---
+++ work_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int number = 1;
+for (int i = 1; i <= N; i++) // если N не включено,то i < N
+{
+    number *= i;
+}
+ 
+Console.WriteLine("Произведение: " + number);
