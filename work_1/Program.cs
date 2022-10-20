3099269ed7169ee513fcd299b8725a268b61fed6
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-20 16:34:42 +0300

    Возводим кубы чисел от 1 до N

---
+++ work_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+for (i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i} ^ 3 = " + i * i * i);
+}
